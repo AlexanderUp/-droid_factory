af9b99c26d0a764311732c77e65dba1cbe5b7bb2
AlexanderUp <alexander.uperenk0@yandex.ru>
2023-01-29 15:05:19 +0300

    Reporting added

--- R4C/robots/utils.py
+++ R4C/robots/utils.py
@@ -1,5 +1,6 @@
 import os
 from datetime import timedelta
+from fnmatch import fnmatch
 from tempfile import TemporaryDirectory
 
 from django.conf import settings
@@ -9,40 +10,39 @@ from django.db.models import Count
 from django.utils import timezone
 from openpyxl import Workbook
 
-from .models import RobotVersion
+from .models import Robot
 
 
 def get_last_robots(days_count=settings.EXCEL_REPORT_DAYS_COUNT):
     today = timezone.now()
-    latest_production_date = (
-        today - timedelta(days=days_count)
-    )
-    return (RobotVersion.objects
-                        .select_related("model")
-                        .prefetch_related("robots")
-                        .values("model__model", "version")
-                        .filter(robots__created__gte=latest_production_date)
-                        .annotate(robots_count=Count("robots")))
+    earliest_production_date = today - timedelta(days=days_count)
+    return (Robot.objects
+                 .filter(created__gte=earliest_production_date)
+                 .values("version__model__model", "version__version")
+                 .annotate(robots_count=Count("version__version")))
 
 
 def get_report_workbook():
     queryset = get_last_robots()
-    models = set(queryset.values_list("model__model", flat=True).all())
+    models = set(queryset.values_list(
+        "version__model__model", flat=True).all())
     wb = Workbook()
     ws = wb.active
     wb.remove_sheet(ws)
     sheet_header = ["Model", "Version", "Weekly count"]
     for model in models:
         ws = wb.create_sheet(title=model)
-        ws.append((sheet_header))
-        versions = queryset.filter(model__model=model)
+        ws.append(sheet_header)
+        versions = queryset.filter(version__model__model=model)
         for version in versions:
-            ws.append([model, version["version"], version["robots_count"]])
+            ws.append(
+                [model, version["version__version"], version["robots_count"]]
+            )
     return wb
 
 
 def get_workbook_name():
-    report_date = timezone.now().strftime("%Y_%m_%d")
+    report_date = timezone.now().strftime("%Y_%m_%d_%H_%M_%S")
     return f"report_{report_date}.xlsx"
 
 
@@ -60,3 +60,11 @@ def get_report():
                 settings.MEDIA_ROOT / "docs" / wb_name, content_file
             )
     return path_to_report
+
+
+def cleanup_file_dir(dir, file_name_pattern):
+    for file in os.listdir(dir):
+        path_to_file = os.path.join(dir, file)
+        if os.path.isfile(path_to_file):
+            if fnmatch(file, file_name_pattern):
+                default_storage.delete(path_to_file)
--- R4C/robots/views.py
+++ R4C/robots/views.py
@@ -1,12 +1,13 @@
-import os
-
 from django.conf import settings
 from django.http import FileResponse
 
-from .utils import get_report
+from .utils import cleanup_file_dir, get_report
 
 
 def download_workbook(request):
+    dir_to_cleanup = settings.MEDIA_ROOT / "docs"
+    report_file_pattern = "report_*.xlsx"
+    cleanup_file_dir(dir_to_cleanup, report_file_pattern)
     rel_path_to_report = get_report()
-    path_to_report = os.path.join(settings.MEDIA_ROOT, rel_path_to_report)
+    path_to_report = settings.MEDIA_ROOT / rel_path_to_report
     return FileResponse(open(path_to_report, "rb"))
