e6bb49767b3994a9a7ef1928bec40f5b14578170
AlexanderUp <alexander.uperenk0@yandex.ru>
2023-09-26 11:44:20 +0300

    API

--- R4C/R4C/urls.py
+++ R4C/R4C/urls.py
@@ -5,7 +5,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('robots/', include('robots.urls', namespace='robots')),
 ]
 
 if settings.DEBUG:
--- R4C/robots/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from robots.views import download_workbook
-
-app_name = 'robots'
-
-urlpatterns = [
-    path('report/', download_workbook, name='download_robot_report'),
-]
